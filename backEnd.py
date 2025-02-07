2d0347601f2d441ffa16f6d53ad341406d2a79a8
Ysk-dev <yusufsertkayaysk@gmail.com>
2025-02-07 20:30:29 +0300

    basic example of flask

--- backEnd.py
+++ backEnd.py
@@ -1,1 +1,11 @@
-print("vfe")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloWorld():
+    return "helloWorldof my mysterious"
+
+if __name__ =="__main__":
+    app.run()
+
